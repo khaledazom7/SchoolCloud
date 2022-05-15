060535b2cbba239336775aab04c2ba8171c47d10
khaled <khaledazo7@gmail.com>
2022-05-15 20:11:22 +0300

    Class Settings 2

--- app/src/main/java/com/amjad/myapplicationschool/model/ClassModel.java
+++ app/src/main/java/com/amjad/myapplicationschool/model/ClassModel.java
@@ -14,15 +14,25 @@ public class ClassModel {
     private String section;
     @SerializedName("sectionEn")
     private String sectionEn;
+    @SerializedName("numberId")
+    private String numberId;
+    @SerializedName("sectionId")
+    private String sectionId;
+    @SerializedName("type")
+    private int type;//0:Number , 1:Section, 2: Name (Number + Section)
+
 
     public ClassModel() {
     }
 
-    public ClassModel( String number, String numberEn, String section, String sectionEn) {
+    public ClassModel(String number, String numberEn, String section, String sectionEn, String numberId, String sectionId, int type) {
         this.number = number;
         this.numberEn = numberEn;
         this.section = section;
         this.sectionEn = sectionEn;
+        this.numberId = numberId;
+        this.sectionId = sectionId;
+        this.type = type;
     }
 
     public String getNumber() {
@@ -55,5 +65,29 @@ public class ClassModel {
 
     public void setSectionEn(String sectionEn) {
         this.sectionEn = sectionEn;
+    }
+
+    public String getNumberId() {
+        return numberId;
+    }
+
+    public void setNumberId(String numberId) {
+        this.numberId = numberId;
+    }
+
+    public String getSectionId() {
+        return sectionId;
+    }
+
+    public void setSectionId(String sectionId) {
+        this.sectionId = sectionId;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 }
--- app/src/main/java/com/amjad/myapplicationschool/ui/activity/admin/settings/ClassSettingsActivity.java
+++ app/src/main/java/com/amjad/myapplicationschool/ui/activity/admin/settings/ClassSettingsActivity.java
@@ -10,19 +10,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.amjad.myapplicationschool.R;
 import com.amjad.myapplicationschool.adapter.ClassSettingsAdapter;
 import com.amjad.myapplicationschool.adapter.UsersAdapter;
 import com.amjad.myapplicationschool.databinding.ActivityClassSettingsBinding;
 import com.amjad.myapplicationschool.model.ClassModel;
+import com.amjad.myapplicationschool.model.Student;
 import com.amjad.myapplicationschool.model.User;
 import com.amjad.myapplicationschool.utils.PreferenceUtils;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
@@ -30,6 +36,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -49,15 +56,12 @@ public class ClassSettingsActivity extends AppCompatActivity {
         createClassNumber();
         getAllClassSection();
         createClassSection();
-        //getAllClassName();
-        //createClassName();
+        getAllClassName();
+        createClassName();
     }
 
     private void getAllClassNumber() {
-        Query query = FirebaseFirestore.getInstance().collection("ClassNumbers");
-        /*query.whereEqualTo("visibility", true)
-                .whereEqualTo("newsStatus", true)
-                .orderBy("date", Query.Direction.DESCENDING);*/
+        Query query = FirebaseFirestore.getInstance().collection("ClassRoom").whereEqualTo("type", 0);
         FirestoreRecyclerOptions<ClassModel> options = new FirestoreRecyclerOptions.Builder<ClassModel>()
                 .setQuery(query, ClassModel.class)
                 .build();
@@ -108,7 +112,7 @@ public class ClassSettingsActivity extends AppCompatActivity {
                             progressBar.setVisibility(View.GONE);
                             return;
                         }
-                        ClassModel classModel = new ClassModel(numberArabicValue, numberEnglishValue, "", "");
+                        ClassModel classModel = new ClassModel(numberArabicValue, numberEnglishValue, "", "", "", "", 0);
                         storClassNumber(classModel);
                     }
                 });
@@ -125,7 +129,7 @@ public class ClassSettingsActivity extends AppCompatActivity {
     }
 
     private void storClassNumber(ClassModel classModel) {
-        FirebaseFirestore.getInstance().collection("ClassNumbers").add(classModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+        FirebaseFirestore.getInstance().collection("ClassRoom").add(classModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
             @Override
             public void onComplete(@NonNull Task<DocumentReference> task) {
                 bottomSheetDialog.dismiss();
@@ -135,7 +139,7 @@ public class ClassSettingsActivity extends AppCompatActivity {
 
     //Class Section
     private void getAllClassSection() {
-        Query query = FirebaseFirestore.getInstance().collection("ClassSection");
+        Query query = FirebaseFirestore.getInstance().collection("ClassRoom").whereEqualTo("type", 1);
         FirestoreRecyclerOptions<ClassModel> options = new FirestoreRecyclerOptions.Builder<ClassModel>()
                 .setQuery(query, ClassModel.class)
                 .build();
@@ -186,7 +190,7 @@ public class ClassSettingsActivity extends AppCompatActivity {
                             progressBar.setVisibility(View.GONE);
                             return;
                         }
-                        ClassModel classModel = new ClassModel("", "", sectionArabicValue, sectionEnglishValue);
+                        ClassModel classModel = new ClassModel("", "", sectionArabicValue, sectionEnglishValue, "", "", 1);
                         storClassSection(classModel);
                     }
                 });
@@ -203,7 +207,7 @@ public class ClassSettingsActivity extends AppCompatActivity {
     }
 
     private void storClassSection(ClassModel classModel) {
-        FirebaseFirestore.getInstance().collection("ClassSection").add(classModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+        FirebaseFirestore.getInstance().collection("ClassRoom").add(classModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
             @Override
             public void onComplete(@NonNull Task<DocumentReference> task) {
                 bottomSheetDialog.dismiss();
@@ -213,7 +217,7 @@ public class ClassSettingsActivity extends AppCompatActivity {
 
     //Class Name
     private void getAllClassName() {
-        Query query = FirebaseFirestore.getInstance().collection("ClassName");
+        Query query = FirebaseFirestore.getInstance().collection("ClassRoom").whereEqualTo("type", 2);
         FirestoreRecyclerOptions<ClassModel> options = new FirestoreRecyclerOptions.Builder<ClassModel>()
                 .setQuery(query, ClassModel.class)
                 .build();
@@ -231,39 +235,56 @@ public class ClassSettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        binding.recyclerViewClassNumber.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        binding.recyclerViewClassNumber.setAdapter(classAdapter);
+        binding.recyclerViewClassName.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        binding.recyclerViewClassName.setAdapter(classAdapter);
         classAdapter.startListening();
     }
 
     private void createClassName() {
-        binding.buttonClassNumber.setOnClickListener(new View.OnClickListener() {
+        binding.buttonClassName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 bottomSheetDialog = new BottomSheetDialog(ClassSettingsActivity.this);
-                @SuppressLint("InflateParams") View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.create_class_num_item, null);
+                @SuppressLint("InflateParams") View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.create_class_name_item, null);
                 progressBar = view.findViewById(R.id.progressBar_id);
                 progressBar.setVisibility(View.GONE);
                 Button create = (Button) view.findViewById(R.id.button_create_opening_game_id);
                 ImageView back = (ImageView) view.findViewById(R.id.image_back_id);
-                EditText numberArabic = view.findViewById(R.id.editTextTextNumberArabic);
-                EditText numberEnglish = view.findViewById(R.id.editTextTextNumberEnglish);
-                String userId = PreferenceUtils.getId(getApplicationContext());
+                Spinner spinnerNumber = view.findViewById(R.id.spinnerNumber);
+                Spinner spinnerSection = view.findViewById(R.id.spinnerSection);
+                TextView textViewCLassName = view.findViewById(R.id.textViewCLassName);
+                ArrayList<String> classModelsNumber = new ArrayList<>();
+                ArrayList<String> classModelsSection = new ArrayList<>();
+                FirebaseFirestore.getInstance()
+                        .collection("ClassRoom")
+                        .whereEqualTo("type", 0)
+                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (int i = 0; i < task.getResult().getDocuments().size(); i++) {
+                            classModelsNumber.add(task.getResult().getDocuments().get(i).toObject(ClassModel.class).getNumber());
+                        }
+                    }
+                });
+                FirebaseFirestore.getInstance()
+                        .collection("ClassRoom")
+                        .whereEqualTo("type", 1)
+                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (int i = 0; i < task.getResult().getDocuments().size(); i++) {
+                            classModelsSection.add(task.getResult().getDocuments().get(i).toObject(ClassModel.class).getSection());
+                        }
+                    }
+                });
+                spinner(classModelsNumber,spinnerNumber);
                 create.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        String numberArabicValue = numberArabic.getText().toString();
-                        String numberEnglishValue = numberEnglish.getText().toString();
+                        String spinnerNumberValue = "";
+                        String spinnerSectionValue = "";
                         progressBar.setVisibility(View.VISIBLE);
-                        if (numberArabicValue.isEmpty() && numberEnglishValue.isEmpty()) {
-                            numberArabic.setError("The first class number is required!");
-                            numberArabic.requestFocus();
-                            numberEnglish.setError("The first class number is required!");
-                            numberEnglish.requestFocus();
-                            progressBar.setVisibility(View.GONE);
-                            return;
-                        }
-                        ClassModel classModel = new ClassModel(numberArabicValue, numberEnglishValue, "", "");
+                        ClassModel classModel = new ClassModel("", "", "", "", spinnerNumberValue, spinnerSectionValue, 2);
                         storClassName(classModel);
                     }
                 });
@@ -280,10 +301,30 @@ public class ClassSettingsActivity extends AppCompatActivity {
     }
 
     private void storClassName(ClassModel classModel) {
-        FirebaseFirestore.getInstance().collection("ClassName").add(classModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+        FirebaseFirestore.getInstance().collection("ClassRoom").add(classModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
             @Override
             public void onComplete(@NonNull Task<DocumentReference> task) {
                 bottomSheetDialog.dismiss();
+            }
+        });
+    }
+
+    private ArrayAdapter<String> adapterSpinnerCurrentClass;
+    String currentClass;
+    private void spinner(ArrayList<String> arrayList, Spinner spinner) {
+        adapterSpinnerCurrentClass = new ArrayAdapter<String>(getApplicationContext(), R.layout.spinner_item,arrayList);
+        //adapterSpinnerCurrentClass.setDropDownViewResource(R.layout.spinner_item_dropdown);
+        spinner.setAdapter(adapterSpinnerCurrentClass);
+        //binding.spinnerCurrentClass.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) getContext());
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentClass = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
